7015fc3d6cc17b3f20535de3c6bdc11971356c2e
anugrah-1-dev <tejomaliki1@gmail.com>
2026-05-24 21:38:35 +0700

    hapus kata pos

--- resources/views/tour.blade.php
+++ resources/views/tour.blade.php
@@ -2076,7 +2076,7 @@
             <div class="footer-right">
                 <a href="#" onclick="openDirectRegistration(); return false;" class="btn-footer" id="btn-open-registration" style="text-decoration: none; display: flex; align-items: center; justify-content: center; gap: 8px;">
                     <svg style="width:22px;height:22px" viewBox="0 0 24 24"><path fill="currentColor" d="M15,14C12.33,14 7,15.33 7,18V20H23V18C23,15.33 17.67,14 15,14M6,10V7H4V10H1V12H4V15H6V12H9V10M15,12A4,4 0 0,0 19,8A4,4 0 0,0 15,4A4,4 0 0,0 11,8A4,4 0 0,0 15,12Z" /></svg>
-                    DAFTAR SEKARANG (POS)
+                    DAFTAR SEKARANG
                 </a>
             </div>
         </div>
